c451349ab7404e93fcc370dfe8649f460e4ad996
MoeRookie <MoeRookie@163.com>
2018-02-08 21:48:47 +0800

    修改闪屏界面Activity文件以及相应布局名称

--- app/src/main/java/com/fangzhang/shoppingmall/activity/SplashActivity.java
+++ app/src/main/java/com/fangzhang/shoppingmall/activity/SplashActivity.java
@@ -1,13 +1,15 @@
-package com.fangzhang.shoppingmall;
+package com.fangzhang.shoppingmall.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.fangzhang.shoppingmall.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
