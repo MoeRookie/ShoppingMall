1495c5e9e946ab1b769d1617f8e14f69c759089b
MoeRookie <MoeRookie@163.com>
2018-02-09 10:17:27 +0800

    Home - 添加首页布局

--- app/src/main/java/com/fangzhang/shoppingmall/activity/HomeActivity.java
+++ app/src/main/java/com/fangzhang/shoppingmall/activity/HomeActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
 
+import com.fangzhang.shoppingmall.R;
+
 /**
  * Created by MoeRookie on 2018/2/8.
  */
@@ -14,8 +16,6 @@ public class HomeActivity extends Activity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-        textView.setText(TAG);
-        setContentView(textView);
+        setContentView(R.layout.activity_home);
     }
 }
