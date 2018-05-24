ddb4cfe27bf19a1238952ddebb0ba24d90b711ce
MoeRookie <MoeRookie@163.com>
2018-05-24 09:47:01 +0800

    HomeFragment - 添加布局

--- app/src/main/java/com/fangzhang/shoppingmall/home/fragment/HomeFragment.java
+++ app/src/main/java/com/fangzhang/shoppingmall/home/fragment/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.fangzhang.shoppingmall.R;
 import com.fangzhang.shoppingmall.base.BaseFragment;
 
 /**
@@ -14,18 +15,15 @@ import com.fangzhang.shoppingmall.base.BaseFragment;
 
 public class HomeFragment extends BaseFragment {
     private static final String TAG = HomeFragment.class.getSimpleName();
-    private TextView textView;
     @Override
     public View initView() {
         Log.e(TAG, "主页视图被初始化了");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_home, null);
+        return view;
     }
 
     @Override
     protected void initData() {
         Log.e(TAG, "主页数据被初始化了");
-        textView.setText(TAG);
     }
 }
