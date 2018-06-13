c9f02c5b131dd58bc53df58c9822d8513e72354d
MoeRookie <MoeRookie@163.com>
2018-06-13 11:29:41 +0800

    商品详情 - 设置显示数据

--- app/src/main/java/com/fangzhang/shoppingmall/app/GoodsInfoActivity.java
+++ app/src/main/java/com/fangzhang/shoppingmall/app/GoodsInfoActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -13,8 +15,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.fangzhang.shoppingmall.R;
 import com.fangzhang.shoppingmall.home.bean.GoodsBean;
+import com.fangzhang.shoppingmall.utils.ConstantValue;
 
 /**
  * Created by MoeRookie on 2018/5/31.
@@ -48,9 +52,53 @@ public class GoodsInfoActivity extends Activity implements View.OnClickListener{
         // 接收数据
         goodsBean = getIntent().getParcelableExtra("goods_bean");
         if (goodsBean != null) {
-            Toast.makeText(this, "goodsBean = " + goodsBean.toString(), Toast.LENGTH_SHORT).show();
+            setDataForView(goodsBean);
         }
     }
+    
+    /**
+     * 添加显示数据
+     * @param goodsBean 要显示的数据
+     */
+    private void setDataForView(GoodsBean goodsBean) {
+        // 图片
+        Glide.with(this)
+                .load(ConstantValue
+                .IMAGE_BASE_URL+goodsBean.getFigure())
+                .into(ivGoodInfoImage);
+        // 商品名称
+        tvGoodInfoName.setText(goodsBean.getName());
+        // 商品价格
+        tvGoodInfoDesc.setText("￥"+goodsBean.getCover_price());
+        // 配置WebView加载的网页
+        setWebViewData(goodsBean.getProduct_id());
+    }
+    
+    /**
+     * 设置要显示的网页数据
+     * @param product_id
+     */
+    private void setWebViewData(String product_id) {
+        if (product_id != null) {
+            wbGoodInfoMore.loadUrl("http://www.atguigu.com");
+            // 设置支持Js
+            WebSettings webSettings = wbGoodInfoMore.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            // 支持双击页面变大变小
+            webSettings.setUseWideViewPort(true);
+            // 优先使用缓存
+            webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+            wbGoodInfoMore.setWebViewClient(new WebViewClient(){
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
+        }
+    }
+    
     /**
      * Find the Views in the layout<br />
      * <br />
